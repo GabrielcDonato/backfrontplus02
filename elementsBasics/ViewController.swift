2307afad63dd05e1d3b2be9b78b876e60fef530a
Gabriel Donato <premiersoft-gabriel.donato@ailos.coop.br>
2023-06-13 22:26:16 -0300

    feat: ibaction to set actions in a component, a press in button to example

--- elementsBasics/ViewController.swift
+++ elementsBasics/ViewController.swift
@@ -14,16 +14,22 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var bemVindoLabel: UILabel!
     
+    @IBOutlet weak var loginButton: UIButton!
     
-        
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         bemVindoLabel.text = "Teste UILABEL"
+        loginButton.backgroundColor = .black
     }
 
-
+    @IBAction func tappedButton(_ sender: UIButton) {
+        print("Botão pressionado!!")
+        view.backgroundColor = .orange
+    }
+    
 }
 
