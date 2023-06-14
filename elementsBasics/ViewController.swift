33dc31b32855c4c85ad3f48aff2e3675be4a082d
Gabriel Donato <premiersoft-gabriel.donato@ailos.coop.br>
2023-06-13 21:39:10 -0300

    Initial Commit

---
+++ elementsBasics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  elementsBasics
+//
+//  Created by Premiersoft on 13/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
