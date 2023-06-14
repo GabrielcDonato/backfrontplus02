2f9ff550ad7ca1fe1d0a2ceb3e8dc4dc9871b34b
Gabriel Donato <premiersoft-gabriel.donato@ailos.coop.br>
2023-06-13 22:11:46 -0300

    feat: ligações iboutlet to define properties of components with coding

--- elementsBasics/ViewController.swift
+++ elementsBasics/ViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    
+    @IBOutlet weak var bemVindoLabel: UILabel!
+    
+    
+        
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        bemVindoLabel.text = "Teste UILABEL"
     }
 
 
